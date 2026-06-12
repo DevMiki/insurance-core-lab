8e2033ea530dfa7bcd5615d2928a9654e11b5906
Michele Marino <pawgrammer.collie@gmail.com>
2026-06-12 17:45:13 +0200

    :DEV: add QuoteController

---
+++ src/main/java/com/codercollie/insurance_lab_core/controller/QuoteController.java
@@ -0,0 +1,37 @@
+package com.codercollie.insurance_lab_core.controller;
+
+import com.codercollie.insurance_lab_core.dto.quote.CreateQuoteRequest;
+import com.codercollie.insurance_lab_core.dto.quote.QuoteResponse;
+import com.codercollie.insurance_lab_core.service.QuoteService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/quotes")
+public class QuoteController {
+
+    private final QuoteService quoteService;
+
+    public QuoteController(QuoteService quoteService) {
+        this.quoteService = quoteService;
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public QuoteResponse createQuote(@Valid @RequestBody CreateQuoteRequest createQuoteRequest) {
+        return quoteService.createQuote(createQuoteRequest);
+    }
+
+    @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public QuoteResponse getQuoteById(@PathVariable Long id) {
+        return quoteService.getQuoteById(id);
+    }
+}
