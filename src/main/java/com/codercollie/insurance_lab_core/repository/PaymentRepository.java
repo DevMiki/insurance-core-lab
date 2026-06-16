6ad7e5b021ef5825b8997978309ea4b9a43fb39f
Michele Marino <pawgrammer.collie@gmail.com>
2026-06-16 17:03:47 +0200

    :DEV: add PaymentRepository

---
+++ src/main/java/com/codercollie/insurance_lab_core/repository/PaymentRepository.java
@@ -0,0 +1,13 @@
+package com.codercollie.insurance_lab_core.repository;
+
+import com.codercollie.insurance_lab_core.persistence.entity.PaymentEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface PaymentRepository extends JpaRepository<PaymentEntity, Long> {
+
+    boolean existsByExternalReference(String externalReference);
+
+    List<PaymentEntity> findByPolicyIdOrderByPaymentDateAscIdAsc(Long policyId);
+}
