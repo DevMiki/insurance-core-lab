688caec39d5722ee1823ebee638784d87797627a
Michele Marino <pawgrammer.collie@gmail.com>
2026-06-19 15:58:40 +0200

    :DEV: add getClaimById method

--- src/main/java/com/codercollie/insurance_lab_core/controller/ClaimController.java
+++ src/main/java/com/codercollie/insurance_lab_core/controller/ClaimController.java
@@ -5,6 +5,8 @@ import com.codercollie.insurance_lab_core.dto.claim.CreateClaimRequest;
 import com.codercollie.insurance_lab_core.service.ClaimService;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,6 +21,11 @@ public class ClaimController {
 
     public ClaimController(ClaimService claimService) {
         this.claimService = claimService;
+    }
+
+    @GetMapping("/claims/{id}")
+    public ClaimResponse getClaimById(@PathVariable Long id) {
+        return claimService.getClaimById(id);
     }
 
     @PostMapping("/claims")
